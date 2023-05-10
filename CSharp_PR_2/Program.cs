8852e82cc3c95de5cd48027a4826bbd20a3affb6
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 10:41:58 +0530

    add : Program.cs
    this file have the main method but here i used a higher order statement so main method are hidden.

--- CSharp_PR_2/Program.cs
+++ CSharp_PR_2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using CSharp_PR_2;
+
+Customer_Account abhi_patel = new("Abhi Patel", "8532 5858 7412");
+abhi_patel.SetBackName("Federal Bank");
+
+Customer_Account parthiv_hirani = new("Parthiv Hirani", "7436 8912 6525");
+parthiv_hirani.SetBackName("SBI");
+
+abhi_patel.PrintInfo();
+parthiv_hirani.PrintInfo();

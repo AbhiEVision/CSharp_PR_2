85b6fb2139c6e9d6c240ecce6611644783ebfc2a
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 10:40:41 +0530

    add : Customer_Account.cs
    this class file contains detail about customer.

---
+++ CSharp_PR_2/Customer_Account.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharp_PR_2
+{
+	public class Customer_Account
+	{
+		private string bank_name;
+		private string customer_accountNo;
+		private string customer_name;
+
+		public Customer_Account(string name, string accountNo)
+		{
+			customer_accountNo = accountNo;
+			customer_name = name;
+		}
+
+		public void PrintInfo()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("--------------------------------------------------------");
+			Console.WriteLine();
+			Console.WriteLine($"Customer name is {customer_name}");
+			Console.WriteLine($"Customer account number is {customer_accountNo}");
+			Console.WriteLine($"Customer bank name is {bank_name}");
+			Console.WriteLine();
+			Console.WriteLine("-------------------------------------------------------");
+			Console.ResetColor();
+		}
+
+		public void SetBackName(string backName)
+		{
+			bank_name = backName;
+		}
+
+	}
+}

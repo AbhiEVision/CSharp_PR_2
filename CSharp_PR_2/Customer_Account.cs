bfafff7344dd7d1099e511f82d6f04f66c04a6f8
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 10:49:31 +0530

    add : summary above the printInfo() method

--- CSharp_PR_2/Customer_Account.cs
+++ CSharp_PR_2/Customer_Account.cs
@@ -12,16 +12,17 @@
 			customer_name = name;
 		}
 
+		// this method first make color green after that they will print the details of user like their name, account no. and bank name.
 		public void PrintInfo()
 		{
 			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("--------------------------------------------------------");
+			Console.WriteLine("================ Customer bank details ================");
 			Console.WriteLine();
 			Console.WriteLine($"Customer name is {customer_name}");
 			Console.WriteLine($"Customer account number is {customer_accountNo}");
 			Console.WriteLine($"Customer bank name is {bank_name}");
 			Console.WriteLine();
-			Console.WriteLine("-------------------------------------------------------");
+			Console.WriteLine("=======================================================\n");
 			Console.ResetColor();
 		}
 
